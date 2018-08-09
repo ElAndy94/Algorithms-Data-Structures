5ec6d3b44c5e8e3d867de4b5351d342e79f6b9ea
Andy <andrewpeliza@hotmail.com>
2018-08-09 13:59:40 +0100

    Logical Steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,8 +17,52 @@
 //       '### '
 //       '####'
 
-function steps(n) {
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
+  }
 
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+  steps(n, row, stair);
+}
+//////////////////////////////////
+
+/// Below prints from 10 until its 0
+function printNum(n, dec = 1) {
+  if (n === 0) {
+    return;
+  }
+
+  // console.log(n);
+  printNum(n - dec);
+}
+
+printNum(10);
+
+//////////////////////////////
+
+function steps(n) {
+ for (let row = 0; row < n; row++) {
+   let stair = '';
+
+   for (let column = 0; column < n; column++) {
+     if (column <= row) {
+       stair += '#';
+     } else {
+       stair += ' ';
+     }
+   }
+  //  console.log(stair);
+ }
 }
 
 module.exports = steps;
