68ded0618e1e0c52a32e94acad18c8333b1fe00b
Andy <andrewpeliza@hotmail.com>
2018-08-10 10:33:26 +0100

    All fibonacci scenarios (with the boosted version)

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,42 @@
 // Example:
 //   fib(4) === 3
 
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+function slowfib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoize(slowfib);
+
+//////////////////////////
+
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+///////////////////////////////////
+
 function fib(n) {
   const result = [0, 1];
 
